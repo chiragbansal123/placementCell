9eb9e7c00664d3d5531ae69356101ed89cf6dd7b
chiragbansal123 <47918090+chiragbansal123@users.noreply.github.com>
2023-04-26 15:13:34 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const env=require('./environment');
 
-mongoose.connect(`mongodb+srv://chiragbansal123:O3b4811jzoCDcWyW@cluster0.lne9m.mongodb.net/test?retryWrites=true&w=majority`);
+mongoose.connect(`mongodb+srv://chiragbansal123:OPhfrCvcYpV5F9pM@cluster0.lne9m.mongodb.net/test?retryWrites=true&w=majority`);
 
 const db = mongoose.connection;
 
